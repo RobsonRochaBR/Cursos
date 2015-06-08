4b2f7f320874e01547406a89579a747f398764c0
Robson Rocha - Consultoria e Treinamento <contato@robsonrocha.com.br>
2015-06-08 05:14:28 -0300

    Versão inicial do walkthrough da aula 1

---
+++ ASP.NET MVC/Aula 1/_Final/Agenda.Web/Models/CadastrarEditarTurmaModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Agenda.Dados;
+
+namespace Agenda.Web.Models
+{
+    public class CadastrarEditarTurmaModel
+    {
+        public TurmaModel Turma { get; set; }
+        public IEnumerable<Curso> Cursos { get; set; }
+        public IEnumerable<Professor> Professores { get; set; }
+    }
+}
